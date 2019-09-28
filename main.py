2db93af5e874ea052cb01ddc0b6f5fac9690505d
Levente Antaloczy <levente525@gmail.com>
2019-09-29 01:57:48 +0200

    Modulokra bontva

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from builtins import input
 
 from dataStructure import *
 from mappingMpvement import *
+from constans import *
 
 BP = brickpi3.BrickPi3()
 
@@ -12,14 +13,12 @@ move = Movement()
 try:
 	for y in range(h):
 		for x in range(w):					
-			move.motorRotateDegree(BP.PORT_D, BP.PORT_A, 230, -50, BP)																#forward 10cm with 230 degree
+			move.motorRotateDegree(BP.PORT_D, BP.PORT_A, wheelRotateDegree, speed, BP)																#forward 10cm with 230 degree
 			time.sleep(0.2)
 			try:
 				value = BP.get_sensor(BP.PORT_1)																					#TODO: színszenzor itt olvasson be a mx-ba 															
 				Matrix[y][x] = value
-				print("X: ", x)
-				print("Y:_", y)
-				print("Matrix elemei: ", Matrix[y][x])
+
 			except brickpi3.SensorError as error:
 				print(error)
 except KeyboardInterrupt:																											#TODO forduljon az x iteráció után
